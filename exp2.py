62a3490189b62886aa14f04f1635556ff03a4467
roshinipushpika <roshinipushpikachowdary@gmail.com>
2026-01-06 23:37:14 +0530

    Update exp2.py

--- exp2.py
+++ exp2.py
@@ -24,4 +24,14 @@ num2 = float(input("Enter second number: "))
 result = num1 * num2
 
 print("Product =", result)
+# Simple Division Program
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if num2 != 0:
+    result = num1 / num2
+    print("Quotient =", result)
+else:
+    print("Error: Division by zero is not allowed")
 
