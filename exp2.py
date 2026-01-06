8819531ed140a13536f19a77d734cbda9dae5c89
kavyathatavarthi <kavyathatavarthi@gmail.com>
2026-01-06 23:39:18 +0530

    Rename application and streamline input prompts
    Updated application name and removed redundant input prompts for subtraction, multiplication, and division.

--- exp2.py
+++ exp2.py
@@ -1,5 +1,4 @@
-print("Collaborative Application")
-print("change 1")
+print("Collaborative Calculator Application")
 print("Addition operator")
 # Simple Addition Program
 num1 = float(input("Enter first number: "))
@@ -10,24 +9,17 @@ result = num1 + num2
 print("Sum =", result)
 # Simple Subtraction Program
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
 result = num1 - num2
 
 print("Difference =", result)
 # Simple Multiplication Program
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
 
 result = num1 * num2
 
 print("Product =", result)
 # Simple Division Program
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
 
 if num2 != 0:
     result = num1 / num2
